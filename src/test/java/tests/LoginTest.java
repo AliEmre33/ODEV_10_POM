77927a277d848ae163850935b15657bbc6277c1c
AliEmre33 <aliemrebulbul@gmail.com>
2025-03-10 22:37:07 +0300

    eksik page ve dosyalar eklendi

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,4 +1,25 @@
 package tests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+public class LoginTest extends BaseTest {
 
-public class LoginTest {
+    @Test
+    public void testInvalidLogin() {
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("invalid_user", "invalid_pass");
+
+
+        String actualError = loginPage.getErrorMessage();
+
+        String expectedError = "Kullanıcı adi ve sifre uyusmuyor";
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        Assert.assertEquals(actualError, expectedError,
+                "Hata mesajı beklenenden farklı!");
+    }
 }
